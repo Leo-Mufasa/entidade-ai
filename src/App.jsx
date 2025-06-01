397b4182431f3b57cfbe9074494b32c25592a8a3
LeoMufasa <reinaldo.hartmann@gmail.com>
2025-06-01 17:36:14 -0400

    agora sim, entidade presente

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <main className="flex items-center justify-center h-screen bg-black text-white">
+      <div className="text-center">
+        <h1 className="text-2xl md:text-4xl font-semibold tracking-widest uppercase">
+          Você chegou até aqui. Isso é tudo.
+        </h1>
+      </div>
+    </main>
+  );
+}
