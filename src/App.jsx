5cdc5fa5769b3bc46203b35eff8b498b37b891a3
LeoMufasa <reinaldo.hartmann@gmail.com>
2025-06-01 23:06:47 -0400

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,25 @@ export default function App() {
       <img
         src="/corrente.png"
         alt="Corrente"
-        className="absolute bottom-40 md:bottom-52 left-1/2 -translate-x-1/2 w-[120vw] max-w-[1000px] object-contain z-10 animate-vibrate pointer-events-none"
+        className="absolute bottom-60 md:bottom-72 left-1/2 -translate-x-1/2 w-[130vw] max-w-[1100px] object-contain z-10 animate-vibrate pointer-events-none"
       />
 
-      {/* Pássaros mais altos */}
+      {/* Texto ENTIDADE */}
+      <img
+        src="/entidade-texto.png"
+        alt="ENTIDADE"
+        className="absolute bottom-2 left-1/2 -translate-x-1/2 w-[60vw] max-w-[400px] object-contain z-20 pointer-events-none"
+      />
+
+      {/* Pássaros bem mais altos */}
       <img
         src="/passaros-flutuando.png"
         alt="Pássaros"
-        className="absolute bottom-[38%] md:bottom-[42%] left-1/2 w-[100vw] max-w-[720px] object-contain z-30 animate-fly pointer-events-none"
+        className="absolute bottom-[55%] md:bottom-[60%] left-1/2 w-[100vw] max-w-[720px] object-contain z-30 animate-fly pointer-events-none"
       />
 
-      {/* Frase ajustada */}
-      <div className="absolute bottom-[57%] md:bottom-[50%] left-1/2 -translate-x-1/2 z-40 px-4">
+      {/* Frase central */}
+      <div className="absolute bottom-[65%] md:bottom-[58%] left-1/2 -translate-x-1/2 z-40 px-4">
         <h1 className="text-black text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold tracking-widest uppercase max-w-md text-center drop-shadow-md leading-snug">
           Você chegou até aqui.<br />Isso é tudo.
         </h1>
