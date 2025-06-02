2b55cda7a584be4d224717bdb8c3a926ae96c45c
LeoMufasa <reinaldo.hartmann@gmail.com>
2025-06-01 21:35:53 -0400

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,29 @@ export default function App() {
         className="absolute inset-0 w-full h-full object-cover z-0"
       />
 
-      {/* Pássaros reposicionados mais perto da corrente */}
-      <img
-        src="/passaros.png"
-        alt="Pássaros"
-        className="absolute top-[15%] left-1/2 -translate-x-1/2 w-[90vw] max-w-[600px] object-contain z-10 pointer-events-none"
-      />
-
-      {/* Corrente ancorada visualmente */}
+      {/* Corrente fixa no rodapé */}
       <img
         src="/corrente.png"
         alt="Corrente"
-        className="absolute bottom-24 left-1/2 -translate-x-1/2 w-[90vw] max-w-[700px] object-contain z-20 pointer-events-none"
+        className="absolute bottom-16 md:bottom-24 left-1/2 -translate-x-1/2 w-[90vw] max-w-[700px] object-contain z-10 pointer-events-none"
       />
 
-      {/* Texto ENTIDADE */}
+      {/* Pássaros acima da corrente, como se emergissem */}
+      <img
+        src="/passaros.png"
+        alt="Pássaros"
+        className="absolute bottom-[40%] md:bottom-[42%] left-1/2 -translate-x-1/2 w-[70vw] max-w-[500px] object-contain z-20 pointer-events-none"
+      />
+
+      {/* Texto ENTIDADE no rodapé, fixo abaixo da corrente */}
       <img
         src="/entidade-texto.png"
         alt="ENTIDADE"
         className="absolute bottom-4 left-1/2 -translate-x-1/2 w-[60vw] max-w-[400px] object-contain z-30 pointer-events-none"
       />
 
-      {/* Frase reposicionada entre pássaros e corrente */}
-      <div className="absolute top-[45%] left-1/2 -translate-x-1/2 z-40 px-4">
+      {/* Frase entre corrente e pássaros */}
+      <div className="absolute bottom-[28%] md:bottom-[30%] left-1/2 -translate-x-1/2 z-40 px-4">
         <h1 className="text-black text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold tracking-widest uppercase max-w-md text-center drop-shadow-md">
           Você chegou até aqui. Isso é tudo.
         </h1>
