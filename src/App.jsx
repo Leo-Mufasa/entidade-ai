202ee5f341f9ce917a68862e2663c1d3e6ea196e
LeoMufasa <reinaldo.hartmann@gmail.com>
2025-06-01 22:54:06 -0400

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,20 @@ export default function App() {
       <img
         src="/corrente.png"
         alt="Corrente"
-        className="absolute bottom-10 md:bottom-16 left-1/2 -translate-x-1/2 w-[90vw] max-w-[700px] object-contain z-10 animate-vibrate pointer-events-none"
+        className="absolute bottom-24 md:bottom-32 left-1/2 -translate-x-1/2 w-[100vw] max-w-[720px] object-contain z-10 animate-vibrate pointer-events-none"
       />
 
-      {/* Texto ENTIDADE */}
-      <img
-        src="/entidade-texto.png"
-        alt="ENTIDADE"
-        className="absolute bottom-2 left-1/2 -translate-x-1/2 w-[60vw] max-w-[400px] object-contain z-20 pointer-events-none"
-      />
 
       {/* Pássaros maiores com animação de flutuação */}
       <img
         src="/passaros-flutuando.png"
         alt="Pássaros"
-        className="absolute bottom-[48%] left-1/2 w-[90vw] max-w-[640px] object-contain z-30 animate-fly pointer-events-none"
+        className="absolute bottom-[28%] left-1/2 w-[100vw] max-w-[720px] object-contain z-30 animate-fly pointer-events-none"
       />
 
       {/* Frase central */}
-      <div className="absolute bottom-[35%] md:bottom-[38%] left-1/2 -translate-x-1/2 z-40 px-4">
-        <h1 className="text-black text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold tracking-widest uppercase max-w-md text-center drop-shadow-md leading-relaxed">
+      <div className="absolute bottom-[47%] md:bottom-[38%] left-1/2 -translate-x-1/2 z-40 px-4">
+        <h1 className="text-black text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold tracking-widest uppercase max-w-md text-center drop-shadow-md leading-relaxed">
           Você chegou até aqui.<br />Isso é tudo.
         </h1>
       </div>
