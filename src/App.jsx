fd396818ba115ad85ea0f8afc19eae12de7ab873
LeoMufasa <reinaldo.hartmann@gmail.com>
2025-06-01 20:29:22 -0400

    fundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
-    <main className="flex items-center justify-center h-screen bg-black text-white">
-      <div className="text-center">
+    <main className="flex items-center justify-center h-screen text-black relative">
+      <div className="absolute inset-0 bg-cover bg-center opacity-80 z-0" style={{ backgroundImage: 'url("/bg-entidade.png")' }}></div>
+      <div className="text-center z-10">
         <h1 className="text-2xl md:text-4xl font-semibold tracking-widest uppercase">
           Você chegou até aqui. Isso é tudo.
         </h1>
