636d9c7c8d2a4997f525e16e19db72614fd310e4
LeoMufasa <reinaldo.hartmann@gmail.com>
2025-06-19 03:19:41 -0400

    update 2025-06-19_031941

--- entidade-ai-vercel/api/add-to-sheet.js
+++ entidade-ai-vercel/api/add-to-sheet.js
@@ -1,7 +1,7 @@
 
 const fetch = require('node-fetch');
 
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzTr3XPROR3pIlMvGHmN5zO9FCsQBkch5PCPO8NE0ZWOeE1ReTdg4PiU6-539x23TjJ/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz30Tj7C_NIBvrP3FrRM1mgIxYGFgHu5rOAVGKQwwM43OJb2gr0P1qZ-wNra9FfaxXm/exec';
 
 module.exports = async (req, res) => {
   if (req.method !== 'POST') {
